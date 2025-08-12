31717a82b916f3883ff38a2ea3ea9cc90a92e10e
DELL <DELL@DAYANNASIXCO>
2025-08-12 17:07:27 -0600

    Colaboradores

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AnalisisGrupal.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AnalisisGrupal.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalisisGrupal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+49291170cdf0297543ca73837a4594899431aa64")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalisisGrupal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalisisGrupal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
